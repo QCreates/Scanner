9a617470117f480cfd639d00e218695530c53272
Qasem Abdeljaber <qasem.abdeljaber@student.allenisd.org>
2023-04-25 11:51:46 -0500

    forgot what I did tbh

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,20 @@
+//const fs require('fs');
+
+function addUser(){
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+
+    var data = {
+	"email":email.value,
+	"password":password.value
+    }
+    //convert JavaScript object to JSON
+    var userData = JSON.stringify(data)
+//    fs.writeFile('users.json', userData)
+
+    window.location.replace("https://codermerlin.com/users/qasem-abdeljaber/Scanner/Frontend/login.html");
+}
+
 function showPassword() {
     var x = document.getElementById("password");
     if (x.type === "password") {
